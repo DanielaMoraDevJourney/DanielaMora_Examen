abf9ccc5e114d47681cbca1511ab5de1e8df6185
Danie <Danie@MORTICIAADDAMS>
2024-04-24 10:53:04 -0500

    Agregar archivos de proyecto.

---
+++ DanielaMora_Examen/Models/DM_tabla1.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DanielaMora_Examen.Models
+{
+    public class DM_tabla1
+    {
+        public int IdDm { get; set; }
+
+        [StringLength(10, MinimumLength = 5)]
+        [Required(AllowEmptyStrings = true)]
+
+        public String? Nombres { get; set; }
+        public  String? Apellidos { get; set; }
+
+        [Required]
+        public bool CuentaAhorro { get; set; }
+        public bool CuentaCorriente { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime CitaConsulta { get; set; }
+
+        [Range(0.01, 9999.99)]
+        public decimal DineroDisponible { get; set; }
+
+     
+
+
+
+
+
+    }
+}
